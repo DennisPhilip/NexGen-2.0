2fb5ea473b73eca952dc9cb1c9e7f2ddf7948a88
DennisPhilip <dennisphilip.gg@gmail.com>
2025-01-28 01:19:21 +0530

    added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
             <main className="flex-grow">
                 <Navbar />
                 <Hero />
-                <LogoTicker />
                 <DevfolioBrandLogos />
+                <LogoTicker />
                 <Features />
                 <Tracks />
                 <Faqs />
