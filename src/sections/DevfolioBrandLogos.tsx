4e916a9363d57b5a51f2baac5c25b907ac63ad98
DennisPhilip <dennisphilip.gg@gmail.com>
2025-01-28 01:38:32 +0530

    added logos

--- src/sections/DevfolioBrandLogos.tsx
+++ src/sections/DevfolioBrandLogos.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import devfolioLogo from "@/assets/images/Devfolio_Logo-White.png";
 import polygonLogo from "@/assets/images/Polygon_Logo-White.png";
 import ethindiaLogo from "@/assets/images/ethindia-light.png";
@@ -8,25 +7,25 @@ export default function DevfolioBrandLogos() {
   return (
     <section className="pb-24 overflow-x-clip">
       <div className="flex flex-row items-center justify-center">
-        <Image
-          src={devfolioLogo}
+        <img
+          src={devfolioLogo.src}
           alt="DEVFOLIO LOGO"
-          width={192} 
-          height={48} 
+          width="192"
+          height="48"
           className="mx-auto"
         />
-        <Image
-          src={polygonLogo}
+        <img
+          src={polygonLogo.src}
           alt="POLYGON LOGO"
-          width={192} 
-          height={48} 
+          width="192"
+          height="48"
           className="mx-auto"
         />
-        <Image
-          src={ethindiaLogo}
+        <img
+          src={ethindiaLogo.src}
           alt="ETHINDIA LOGO"
-          width={192} 
-          height={48} 
+          width="192"
+          height="48"
           className="mx-auto"
         />
       </div>
