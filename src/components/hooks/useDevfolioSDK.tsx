dcf8946fe6b6ff2d7fc4276a4ee23ced1b86e837
Yathartha19 <aarushyathartha19@gmail.com>
2025-01-27 22:21:12 +0530

    devfolio integration

---
+++ src/components/hooks/useDevfolioSDK.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+const useDevfolioSDK = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://apply.devfolio.co/v2/sdk.js";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  },[]);
+};
+
+export default useDevfolioSDK;
---
+++ src/sections/DevfolioBrandLogos.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Image from "next/image";
+import devfolioLogo from "@/assets/images/Devfolio_Logo-White.png";
+import polygonLogo from "@/assets/images/Polygon_Logo-White.png";
+import ethindiaLogo from "@/assets/images/ethindia-light.png";
+
+export default function DevfolioBrandLogos() {
+  return (
+    <section className="pb-24 overflow-x-clip">
+      <div className="flex flex-row items-center justify-center">
+        <Image
+          src={devfolioLogo}
+          alt="Devfolio"
+          width={192} 
+          height={48} 
+          className="mx-auto"
+        />
+        <Image
+          src={polygonLogo}
+          alt="Devfolio"
+          width={192} 
+          height={48} 
+          className="mx-auto"
+        />
+        <Image
+          src={ethindiaLogo}
+          alt="Devfolio"
+          width={192} 
+          height={48} 
+          className="mx-auto"
+        />
+      </div>
+    </section>
+  );
+}
